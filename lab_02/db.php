3dadf4b2c7cfa8a0218840f297e03c2de794dab7
aliahmadcse <aliahmadcse@gmail.com>
2019-12-12 21:19:08 +0500

    Applies validations successfully

--- lab_02/db.php
+++ lab_02/db.php
@@ -4,8 +4,8 @@ $user_name = "root";
 $password = "";
 $db_name = "my_db";
 
-$conn = new mysqli($host, $user_name, $password, $db_name);
+$conn = mysqli_connect($host, $user_name, $password, $db_name);
 
-if ($conn->connect_error) {
-    die("Connection failed" . $conn->connect_error);
+if (!$conn) {
+    die("Connection Error: " . mysqli_connect_error());
 }
--- lab_02/process.php
+++ lab_02/process.php
@@ -1,19 +1,30 @@
 <?php
-require "db.php";
+session_start();
+if (isset($_POST["std_form"])) {
+    require "db.php";
 
-$name = $_POST["name"];
-$father_name = $_POST["fatherName"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$country = $_POST["country"];
-$gender = $_POST["gender"];
+    $name = $_POST["name"];
+    $father_name = $_POST["fatherName"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $country = $_POST["country"];
+    $gender = $_POST["gender"];
 
-$sql = "INSERT INTO `students` (`std_name`,`std_father_name`,`std_email`,
-`std_gender`,`std_country`,`std_mobile_number`) VALUES ('$name','$father_name',
-'$email','$gender','$country','$phone')";
+    $sql = "INSERT INTO students (std_name,std_father_name,std_email,
+        std_gender,std_country,std_mobile_number) VALUES ('$name','$father_name',
+        '$email','$gender','$country','$phone')";
 
-if ($conn->query($sql)) {
-    echo "Inserted success!";
+    $result = mysqli_query($conn, $sql);
+    mysqli_close($conn);
+    if (isset($result)) {
+        $_SESSION["msj"] = "Record Inserted Successfully";
+        header("location:index.php");
+    } else {
+        $_SESSION["msj"] = "Error: " . $sql . "<br />" . mysqli_error($conn);
+        header("location:index.php");
+    }
 } else {
-    echo $conn->error;
+    // echo "Invalid access";
+    $_SESSION["msj"] = "Invalid Access";
+    header("location:index.php");
 }
