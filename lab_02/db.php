47d8437a5fd4c056e22b08ed11c18de91527b972
aliahmadcse <aliahmadcse@gmail.com>
2020-01-02 15:26:48 +0500

    Modifies db.php

--- lab_02/db.php
+++ lab_02/db.php
@@ -6,6 +6,7 @@ $db_name = "my_db";
 
 $conn = mysqli_connect($host, $user_name, $password, $db_name);
 
+
 if (!$conn) {
-    die("Connection Error: " . mysqli_connect_error());
+    die("Connection Error:" . mysqli_connect_error());
 }
